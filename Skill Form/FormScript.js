34b608faca58b86fbd62808978d3372c832ec1b3
bhazalerturk <126257372+bhazalerturk@users.noreply.github.com>
2023-03-01 01:27:33 +0300

    Add files via upload

---
+++ Skill Form/FormScript.js
@@ -0,0 +1,41 @@
+﻿
+document.getElementById('submit').onclick = function () {
+    
+    document.getElementById("table").style.display = "block";
+    
+    var table = document.getElementById("table");
+    var row = table.insertRow(1);
+    var skill = row.insertCell(0);
+    var level = row.insertCell(1);
+
+    skill.innerHTML = document.getElementById("skill").value;
+    level.innerHTML = getSelectValue();
+    
+    var chooseTable = document.getElementById('table'), rIndex;
+
+    for (var i = 1; i < chooseTable.rows.length; i++) {
+        chooseTable.rows[i].onclick = function () {
+            rIndex = this.rowIndex;
+            console.log(rIndex);
+            deleteRow(rIndex);
+           
+        }
+    }
+    document.getElementById('skill').value = '';
+    return false;
+
+}
+
+
+
+function getSelectValue() {
+    var selectedValue = document.getElementById("levelList").value;
+    console.log(selectedValue);
+    return selectedValue;
+}
+
+
+function deleteRow(r) {
+    
+    document.getElementById("table").deleteRow(r);
+}
